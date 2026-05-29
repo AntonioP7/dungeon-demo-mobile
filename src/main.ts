c519c18ba13d6f7bfbd615d98fa2f41172bf5199
AntonioP7 <97467004+AntonioP7@users.noreply.github.com>
2026-05-29 12:03:03 +0200

    Add Phaser entrypoint

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import Phaser from 'phaser';
+import './styles.css';
+import { BootScene } from './scenes/BootScene';
+import { GameScene } from './scenes/GameScene';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  backgroundColor: '#101820',
+  pixelArt: true,
+  roundPixels: true,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 360,
+    height: 640,
+  },
+  scene: [BootScene, GameScene],
+};
+
+new Phaser.Game(config);
